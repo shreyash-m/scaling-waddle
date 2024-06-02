d52bf52a7f3dcc88d3db1332445f6a75ddcf872f
Shreyas M <78375444+shreyash-m@users.noreply.github.com>
2024-06-02 17:44:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const mongoose = require("mongoose");
  const dotenv = require("dotenv").config();
  const Stripe = require('stripe')
 
+datasource db {
+  provider = "mongoosedb"
+  url      = env("MONGOOSEDB_URL")
+}
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
@@ -173,6 +178,44 @@ app.post("/create-checkout-session",async(req,res)=>{
      }
 
 })
+
+const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (!user || !profile || !account) return false
+
+      const handle = (profile as GithubProfile).login
+
+      const existingUser = await prisma.user.findFirst({
+        where: {
+          handle,
+        },
+      })
+
+      if (existingUser) return true
+
+      const newUser = await prisma.user.create({
+        data: {
+          handle,
+          provider: account.provider as "github",
+          email: user.email as string,
+        },
+      })
+
+      if (newUser) {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+}
 
 
 // //server is ruuning
